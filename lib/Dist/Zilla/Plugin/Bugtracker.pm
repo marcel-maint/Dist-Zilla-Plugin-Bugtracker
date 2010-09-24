633297766b1b8684a778aee709c13f1f2e5a9e93
Marcel Gruenauer <hanekomu@gmail.com>
2010-09-24 17:27:02 +0200

    make bugtracker URL and email address configurable (idea by Mike Doherty)

--- lib/Dist/Zilla/Plugin/Bugtracker.pm
+++ lib/Dist/Zilla/Plugin/Bugtracker.pm
@@ -4,35 +4,56 @@ use warnings;
 
 package Dist::Zilla::Plugin::Bugtracker;
 
-# ABSTRACT: Automatically sets the bugtracker URL
+# ABSTRACT: Automatically sets the bugtracker URL and mailto
 use Moose;
+use MooseX::Types::URI qw(Uri);
 with 'Dist::Zilla::Role::MetaProvider';
+has web => (
+    is      => 'ro',
+    isa     => Uri,
+    coerce  => 1,
+    default => 'http://rt.cpan.org/Public/Dist/Display.html?Name=%s',
+);
+has mailto => (
+    is      => 'ro',
+    isa     => 'Str',
+);
+
+sub interpolate {
+    local $_ = $_[1];
+    my $value = $_[2];
+    s/(?<!%)%s/$value/g;
+    s/(?<!%)%l/lc $value/eg;
+    s/(?<!%)%U/uc $value/eg;
+    s/%%/%/g;
+    $_;
+}
 
 sub metadata {
     my $self = shift;
-
-    my $web = sprintf('http://rt.cpan.org/Public/Dist/Display.html?Name=%s',
-      $self->zilla->name);
-    my $mailto = sprintf('bug-%s at rt.cpan.org', lc( $self->zilla->name ) );
-
-    return {
-        resources => {
-            bugtracker => {
-                web => $web,
-                mailto => $mailto,
-            }
-        }
-    };
+    my $web = $self->interpolate($self->web, $self->zilla->name);
+    my $mailto = $self->mailto;
+    if (!defined($mailto) && index($web, 'http://rt.cpan.org/') == 0) {
+        $mailto = 'bug-%l at rt.cpan.org';
+    }
+    my $result = {};
+    $result->{resources}{bugtracker}{web} = $web;
+    $result->{resources}{bugtracker}{mailto} = $mailto if defined $mailto;
+    $result;
 }
 __PACKAGE__->meta->make_immutable;
 no Moose;
 1;
 
-=pod
+=begin :prelude
+
+=for stopwords mailto
 
 =for test_synopsis
 1;
 __END__
+
+=end :prelude
 
 =head1 SYNOPSIS
 
@@ -42,17 +63,84 @@ In C<dist.ini>:
 
 =head1 DESCRIPTION
 
-This plugin sets the distribution's bugtracker URL as metadata. This plugin
-assumes you are using the CPAN RT bugtracker and sets those 2 fields:
+This plugin sets the distribution's bugtracker URL and possibly the email
+address as metadata. You can set the C<web> and the C<mailto> values
+independently in the plugin configuration.
 
-  web = http://rt.cpan.org/Public/Dist/Display.html?Name=$dist
-  mailto = bug-$dist at rt.cpan.org
+=method web
 
-Please see L<CPAN::Meta::Spec|http://search.cpan.org/dist/CPAN-Meta/lib/CPAN/Meta/Spec.pm#resources>
+This is the bugtracker URL. It defaults to the CPAN RT URL, so it equivalent to:
+
+    [Bugtracker]
+    web = http://rt.cpan.org/Public/Dist/Display.html?Name=%s
+
+See C<interpolate()> for more information.
+
+=method mailto
+
+This is the optional email address to which bug reports can be sent. If the
+CPAN RT bugtracker is used, the email address defaults to C<bug-%l at
+rt.cpan.org> - see C<interpolate()> for what this means.
+
+If a different bugtracker URL is used, the email address is only included in
+the metadata if it is explicitly given.
+
+In the following examples, assume that the distribution is called C<Foo-Bar>.
+
+Example 1:
+
+    [Bugtracker]
+
+will produce the defaults:
+
+    web:    http://rt.cpan.org/Public/Dist/Display.html?Name=Foo-Bar
+    mailto: bug-foo-bar at rt.cpan.org
+
+Example 2:
+
+    [Bugtracker]
+    web = http://github.com/me/%s/issues
+
+will only produce a C<web> entry, but not a C<mailto> entry:
+
+    web: http://github.com/me/Foo-Bar/issues
+
+Example 3:
+
+    [Bugtracker]
+    mailto = me@example.org
+
+will produce:
+
+    web:    http://rt.cpan.org/Public/Dist/Display.html?Name=Foo-Bar
+    mailto: me@example.org
+
+Example 4:
+
+    [Bugtracker]
+    web = http://github.com/me/%s/issues
+    mailto = bug-%U@example.org
+
+will only produce a C<web> entry, but not a C<mailto> entry:
+
+    web:    http://github.com/me/Foo-Bar/issues
+    mailto: bug-FOO-BAR@example.org
+
+See
+L<CPAN::Meta::Spec|http://search.cpan.org/dist/CPAN-Meta/lib/CPAN/Meta/Spec.pm#resources>
 for more information.
 
-=function metadata
+=method interpolate
 
-Sets the bugtracker URL in the distribution's metadata.
+Both the C<web> and C<mailto> strings are interpolated as follows:
+
+    %s  The distribution name as is (e.g., 'Foo-Bar')
+    %l  The distribution name in lowercase (e.g., 'foo-bar')
+    %U  The distribution name in uppercase (e.g., 'FOO-BAR')
+    %%  A literal '%' sign
+
+=method metadata
+
+Sets the bugtracker URL and possibly email address in the distribution's metadata.
 
 =cut
